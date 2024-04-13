bcc665d186eb8ffa743907a8f71735ac9cef9779
lonegunmanb <lonegunmanb@hotmail.com>
2024-04-13 17:05:16 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,59 +1,104 @@
-data resource k8s {
-  type = "azurerm_kubernetes_cluster"
+from "resource" "azurerm_kubernetes_cluster" "aks" {
+  
 }
 
-fix merge k8s_ignore_change {
-  for_each = data.resource.k8s
+transform "update_in_place" "azurerm_kubernetes_cluster_ignore_changes" {
+  from = from.resource.azurerm_kubernetes_cluster.aks
   lifecycle {
-    ignore_change = lexexp("[microsoft_defender[0].log_analytics_workspace_id]")
+    ignore_changes = "[microsoft_defender[0].log_analytics_workspace_id, ${trimprefix(try(from.lifecycle[0].ignore_changes, "[]"), "[")}"
   }
 }
 
-data resource k8s_node_pool {
-  type = "azurerm_kubernetes_cluster"
+from "resource" "azurerm_kubernetes_cluster_node_pool" aks_node_pool {
+  use_for_each = true
 }
 
-locals {
-  k8s_node_pool_with_for_each = [ for block in data.resource.k8s_node_pool : block if block.for_each_iterator != null ]
+transform "update_in_place" azurerm_kubernetes_cluster_node_pool_tags {
+  from = from.resource.azurerm_kubernetes_cluster_node_pool.aks_node_pool
+  tags = "merge(${try(from.tags, "{}")}, { key = each.key })"
 }
 
-fix merge k8s_node_pool_ignore_change {
-  for_each = local.k8s_node_pool_with_for_each
-  lifecycle {
-    ignore_change = lexexp("[microsoft_defender[0].log_analytics_workspace_id]")
+transform "new_block" "private_endpoints_variable" {
+  block_type = "variable"
+  block_labels = ["private_endpoints"]
+  type = map(object({
+    name                                    = optional(string, null)
+    role_assignments                        = optional(map(object({})), {})
+    lock                                    = optional(object({}), {})
+    tags                                    = optional(map(any), null)
+    subnet_resource_id                      = string
+    private_dns_zone_group_name             = optional(string, "default")
+    private_dns_zone_resource_ids           = optional(set(string), [])
+    application_security_group_associations = optional(map(string), {})
+    private_service_connection_name         = optional(string, null)
+    network_interface_name                  = optional(string, null)
+    location                                = optional(string, null)
+    resource_group_name                     = optional(string, null)
+    ip_configurations = optional(map(object({
+      name               = string
+      private_ip_address = string
+    })), {})
+  }))
+  default     = {}
+  description = <<-DESCRIPTION
+  A map of private endpoints to create on the Key Vault. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+
+  - `name` - (Optional) The name of the private endpoint. One will be generated if not set.
+  - `role_assignments` - (Optional) A map of role assignments to create on the private endpoint. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time. See `var.role_assignments` for more information.
+  - `lock` - (Optional) The lock level to apply to the private endpoint. Default is `None`. Possible values are `None`, `CanNotDelete`, and `ReadOnly`.
+  - `tags` - (Optional) A mapping of tags to assign to the private endpoint.
+  - `subnet_resource_id` - The resource ID of the subnet to deploy the private endpoint in.
+  - `private_dns_zone_group_name` - (Optional) The name of the private DNS zone group. One will be generated if not set.
+  - `private_dns_zone_resource_ids` - (Optional) A set of resource IDs of private DNS zones to associate with the private endpoint. If not set, no zone groups will be created and the private endpoint will not be associated with any private DNS zones. DNS records must be managed external to this module.
+  - `application_security_group_resource_ids` - (Optional) A map of resource IDs of application security groups to associate with the private endpoint. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+  - `private_service_connection_name` - (Optional) The name of the private service connection. One will be generated if not set.
+  - `network_interface_name` - (Optional) The name of the network interface. One will be generated if not set.
+  - `location` - (Optional) The Azure location where the resources will be deployed. Defaults to the location of the resource group.
+  - `resource_group_name` - (Optional) The resource group where the resources will be deployed. Defaults to the resource group of the Key Vault.
+  - `ip_configurations` - (Optional) A map of IP configurations to create on the private endpoint. If not specified the platform will create one. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+    - `name` - The name of the IP configuration.
+    - `private_ip_address` - The private IP address of the IP configuration.
+  DESCRIPTION
+  nullable    = false
+}
+
+from "resource" "azurerm_cognitive_account" "cognitive_account" {
+  use_for_each = false
+}
+
+transform "new_block" "private_endpoints_variable" {
+  block_type = "azurerm_private_endpoint"
+  block_labels = ["this"]
+  for_each = var.private_endpoints
+
+  location            = "${from.resource.azurerm_cognitive_account.cognitive_account.ref}.location"
+  name                = coalesce(each.value.name, "pep-${var.name}")
+  resource_group_name = coalesce(each.value.resource_group_name, azurerm_cognitive_account.this.resource_group_name)
+  subnet_id           = each.value.subnet_resource_id
+  tags                = each.value.tags
+
+  private_service_connection {
+    is_manual_connection           = false
+    name                           = coalesce(each.value.private_service_connection_name, "pse-${var.name}")
+    private_connection_resource_id = "${from.resource.azurerm_cognitive_account.cognitive_account.ref}.id"
+    subresource_names              = ["account"]
   }
-}
+  dynamic "ip_configuration" {
+    for_each = each.value.ip_configurations
 
-data resource cognitive_account {
-  type = "azurerm_cognitive_account"
-}
-
-locals {
-  cognitive_accounts_with_for_each = [ for block in data.resource.cognitive_account : block if block.for_each_iterator != null ]
-}
-
-fix newresource private_endpoint_for_cognitive_account {
-  type = "azurerm_private_endpoint"
-  name = "this"
-  for_each = locals.cognitive_accounts_with_for_each
-  # now `each.value` stands for an `azurerm_cognitive_account` block with `for_each`, for example, `azurerm_cognitive_account.this["eastus"]`
-  # let's say we have：
-  # resource azurerm_cognitive_account this {
-  #   for_each = toset["eastus", "westus"]
-  #   ...
-  #   location = each.value
-  # }
-  # `each.value.for_each_iterator` stands for `for_each = toset["eastus", "westus"]`
-  # exp(each.value.for_each_iterator) stands for `toset["eastus", "westus"]`
-  for_each_iterator = exp(each.value.for_each_iterator)
-  /* So we got such new resource block:
-  resource azurerm_private_endpoint this {
-    for_each = toset["eastus", "westus"]
+    content {
+      name               = ip_configuration.value.name
+      private_ip_address = ip_configuration.value.private_ip_address
+      member_name        = "account"
+      subresource_name   = "account"
+    }
   }
-  */
-  # `each.value` now is `azurerm_cognitive_account.this`, it's a map, so we need `withindex`
-  # `withindex(each.value, each_iterator.value)` now is `azurerm_cognitive_account.this[each.value]`
-  location = addr(withindex(each.value, for_each_iterator.value).location)
-  # `azurerm_cognitive_account.this[each.value].location` is `location = each.value` defined in `azurerm_cognitive_account` block
-  # `addr(withindex(each.value, each_iterator.value).location)` is expression: `azurerm_cognitive_account.this[each.value].location`
+  dynamic "private_dns_zone_group" {
+    for_each = length(each.value.private_dns_zone_resource_ids) > 0 ? ["this"] : []
+
+    content {
+      name                 = each.value.private_dns_zone_group_name
+      private_dns_zone_ids = each.value.private_dns_zone_resource_ids
+    }
+  }
 }
