15c374beaa82be566ef83c7a7c3e0011206d32e7
hezijie <lonegunmanb@hotmail.com>
2024-09-25 16:34:43 +0800

    remove concat_block_body transform block, merge it's feature into update_in_place's new dynamic_block_body attribute

--- pkg/init.go
+++ pkg/init.go
@@ -19,7 +19,6 @@ func registerTransform() {
 	golden.RegisterBlock(new(RemoveBlockContentBlockTransform))
 	golden.RegisterBlock(new(RenameAttributeOrNestedBlockTransform))
 	golden.RegisterBlock(new(RegexReplaceExpressionTransform))
-	golden.RegisterBlock(new(ConcatBlockBodyTransform))
 }
 
 func registerData() {
