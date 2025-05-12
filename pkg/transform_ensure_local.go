84fec2b5ce8494c1e5d0e772a10a969ddfdc9649
hezijie <lonegunmanb@hotmail.com>
2025-05-12 16:10:06 +0800

    add transform.ensure_local block, support replace value for now

---
+++ pkg/transform_ensure_local.go
@@ -0,0 +1,77 @@
+package pkg
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Azure/golden"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+)
+
+var _ Transform = &EnsureLocalTransform{}
+var _ golden.CustomDecode = &EnsureLocalTransform{}
+
+type EnsureLocalTransform struct {
+	*golden.BaseBlock
+	*BaseTransform
+	LocalName        string `hcl:"name" validate:"required"`
+	FallbackFileName string `hcl:"fallback_file_name" validate:"required"`
+	writeBlock       *hclwrite.Block
+	tokens           hclwrite.Tokens
+}
+
+func (u *EnsureLocalTransform) Type() string {
+	return "ensure_local"
+}
+
+func (u *EnsureLocalTransform) Apply() error {
+	u.writeBlock.Body().SetAttributeRaw(u.LocalName, u.tokens)
+	return nil
+}
+
+func (u *EnsureLocalTransform) Decode(block *golden.HclBlock, context *hcl.EvalContext) error {
+	var err error
+	u.LocalName, err = getRequiredStringAttribute("name", block, context)
+	if err != nil {
+		return err
+	}
+	u.FallbackFileName, err = getRequiredStringAttribute("fallback_file_name", block, context)
+	if err != nil {
+		return err
+	}
+	cfg := u.Config().(*MetaProgrammingTFConfig)
+	b, ok := cfg.localBlocks[fmt.Sprintf("local.%s", u.LocalName)]
+	if ok {
+		u.writeBlock = b.WriteBlock
+	}
+	asString, err := getOptionalStringAttribute("value_as_string", block, context)
+	if err != nil {
+		return err
+	}
+	raw, asRaw := block.Attributes()["value_as_raw"]
+	if asString != nil && asRaw {
+		return fmt.Errorf("cannot use both value_as_string and value_as_raw")
+	}
+	if asString != nil {
+		u.tokens, err = stringToHclWriteTokens(*asString)
+		if err != nil {
+			return err
+		}
+	}
+	if asRaw {
+		u.tokens = raw.ExprTokens()
+	}
+	return nil
+}
+
+func (u *EnsureLocalTransform) String() string {
+	content := make(map[string]any)
+	content["id"] = u.Id()
+	content["name"] = u.LocalName
+	content["value"] = string(u.tokens.Bytes())
+	str, err := json.Marshal(content)
+	if err != nil {
+		panic(err.Error())
+	}
+	return string(str)
+}
